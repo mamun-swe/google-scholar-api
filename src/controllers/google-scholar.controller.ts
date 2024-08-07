795f6ac1d9ff37e75c9fcc0249e495f058087f45
mamun-swe <mamun.swe.277@gmail.com>
2024-08-06 20:03:33 -0400

    Changes

--- src/controllers/google-scholar.controller.ts
+++ src/controllers/google-scholar.controller.ts
@@ -1,35 +1,40 @@
-
-import { NextFunction, Request, Response } from "express"
-import { IRequestQuery } from "../interfaces"
-import { findById } from "../services/google-scholar.service"
+import { NextFunction, Request, Response } from "express";
+import { IRequestQuery } from "../interfaces";
+import { findById } from "../services/google-scholar.service";
 
 /* Articals by user ID */
-export const ArticalsByUserId = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        let results: any = []
-        const requestQuery: IRequestQuery = <any>req.query
+export const ArticalsByUserId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let results: any = [];
+    const requestQuery: IRequestQuery = <any>req.query;
 
-        /* Validate request query */
-        if (!requestQuery) {
-            return res.status(200).json({
-                status: true,
-                data: []
-            })
-        }
-
-        /* Search data by scholar user ID */
-        if (requestQuery.scholar_user_id) {
-            results = await findById({ scholar_user_id: requestQuery.scholar_user_id })
-        }
-
-        res.status(200).json({
-            status: true,
-            data: results
-        })
-    } catch (error: any) {
-        if (error) {
-            console.log(error)
-            next(error)
-        }
+    /* Validate request query */
+    if (!requestQuery) {
+      return res.status(200).json({
+        status: true,
+        data: [],
+      });
     }
-}
+
+    /* Search data by scholar user ID */
+    if (requestQuery.scholar_user_id) {
+      results = await findById({
+        scholar_user_id: requestQuery.scholar_user_id,
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      data: results,
+    });
+  } catch (error: any) {
+    if (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+};
